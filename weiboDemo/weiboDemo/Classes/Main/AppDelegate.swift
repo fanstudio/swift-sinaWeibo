c6c2b3f2105cbafce51f24c9ed2d9033bb6cb8af
zhangfan <fanstudio@qq.com>
2016-05-30 16:17:53 +0800

    项目结构搭建

---
+++ weiboDemo/weiboDemo/Classes/Main/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  weiboDemo
+//
+//  Created by 张帆 on 16/5/30.
+//  Copyright © 2016年 fanstudio. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.whiteColor()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ weiboDemo/weiboDemo/Classes/Main/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  weiboDemo
+//
+//  Created by 张帆 on 16/5/30.
+//  Copyright © 2016年 fanstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.redColor()
+    }
+
+
+}
+
